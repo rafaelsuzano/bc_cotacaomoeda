2b94fac3892e9c67c75553601dea3f4ff3e5c7e3
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-01-14 20:33:35 -0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,5 +1,9 @@
 #import pymysql
 import mysql.connector
+
+
+
+
 import json
 class conectar(object):
         global db;
@@ -12,7 +16,7 @@ class conectar(object):
                 password = "Suz@no3001"
                 port =3306
                 try:
-                        self.db = mysql.connect(host=host,
+                        self.db = mysql.connector(host=host,
                                                                 database=database,
                                                                 port=port,
                                                                 user=user,
