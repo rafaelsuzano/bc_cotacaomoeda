7678d03591a141750c79a3af50b90b8d48262f31
Rafael Suzano <rafaelsuzano@hotmail.com>
2025-09-09 10:45:03 -0300

    Update Estudo_WS_Pandas.py

--- Estudo_WS_Pandas.py
+++ Estudo_WS_Pandas.py
@@ -32,7 +32,7 @@ for i in range(1):
     except:
             pass
             #opcional o print para exibir a mensagem de sem cotação
-            #print("Sem cotação  " + data )       
+            print("Sem cotação  " + data )       
 
 
 
@@ -64,6 +64,7 @@ def GeraGrafico(dados):
     
 dados=Filtro(fm)
 GeraGrafico(dados)
+
 
 
 
